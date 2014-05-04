7db5003a27d452af5365c3187b2babeb151ee7dd
Hannyajin <talmo.christian@gmail.com>
2014-05-04 17:49:51 +0300

    refactoring

--- app.js
+++ app.js
@@ -5,48 +5,64 @@ var url = 'tmp.png';
 
 var w = 40;
 var h = 40;
+var n = 0; // subimage index
 
 var Canvas = require('canvas')
 	,	canvas = new Canvas(w, h)
 	, ctx = canvas.getContext('2d');;
 
+// holds the sub image data
 var imgDataBuffers = [];
-var img = new Canvas.Image;
 
-img.onerror = function(err) {
-	throw err;
-};
+/**
+	*	Initialization of the Sprite Sheet
+	*/
+function init() {
+	var img = new Canvas.Image;
 
-img.onload = function() {
-	var i = Math.floor(img.width / w);
-	var j = Math.floor(img.height / h);
+	img.onerror = function(err) {
+		throw err;
+	};
 
-	for (var y = 0; y < j; y++) {
-		for (var x = 0; x < i; x++) {
-			ctx.clearRect(0,0,w,h); // necessary for subimages with transparency
-			ctx.drawImage(img, x * w , y * h, w, h, 0, 0, w, h);
-			imgDataBuffers.push(canvas.toBuffer());
+	img.onload = function() {
+		var i = Math.floor(img.width / w);
+		var j = Math.floor(img.height / h);
+
+		for (var y = 0; y < j; y++) {
+			for (var x = 0; x < i; x++) {
+				ctx.clearRect(0,0,w,h); // necessary for subimages with transparency
+				ctx.drawImage(img, x * w , y * h, w, h, 0, 0, w, h);
+				imgDataBuffers.push(canvas.toBuffer());
+			}
 		}
+		
+		// randomize the subimage
+		n = Math.floor(Math.random() * imgDataBuffers.length);
+		startServer();
 	}
-	
 
-	startServer();
-}
-
-img.src = url;
+	img.src = url;
+};
 
 function startServer() {
 	http.createServer(function(req, res) {
 		if (req.url === '/favicon.ico') {
         res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-        res.end(/* icon content here */);
+        res.end(imgDataBuffers[n], 'binary');
      } else {
-     	var n = Math.floor(Math.random() * imgDataBuffers.length);
-     	res.writeHead(200, {'Content-Type': 'image/png' });
+     	res.writeHead(200, {'Content-Type': 'image/x-icon' });
      	res.end(imgDataBuffers[n], 'binary');
-     	console.log("sent image number: " + n);
+     	// console.log("sent image number: " + n);
+
+			// randomize the subimage
+			n = Math.floor(Math.random() * imgDataBuffers.length);
      }
-	}).listen(3000, '127.0.0.1');
+   }).listen(3000, '127.0.0.1');
 
 	console.log("Server listening at port: 3000");
 }
+
+/**
+	*	Start the app
+	*/
+init();
